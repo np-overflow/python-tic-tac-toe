a988a018c44ae0ff1dbd67ee4d475e2ffae0f6c6
Jeyvian <117579078+PeanutBrrutter@users.noreply.github.com>
2023-05-31 10:43:57 +0800

    Update Python_Nagini_Workshop.py

--- Python_Nagini_Workshop.py
+++ Python_Nagini_Workshop.py
@@ -127,7 +127,7 @@ class TicTacToe:
             '''To do: assign values of row and col from userInput'''
             # <hint: a, b = 0, 1>
             row, col = 
-            if self.board[row][col] == "-":             # fixing spot only if the spot is empty
+            if self.board[row - 1][col - 1] == "-":             # fixing spot only if the spot is empty
                 self.fix_spot(row - 1, col - 1, player)
             if self.player_win(player):             # checking if the current player has won the game
                 print("Player {} wins the game!".format(player))
